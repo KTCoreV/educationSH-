c3e7823f6cd548a034d94b762c4d6749f8cfe538
KTCoreV <cwucore@gmail.com>
2016-09-13 20:27:00 +0300

    Update water.c

--- water.c
+++ water.c
@@ -4,7 +4,7 @@
 int main (void)
 {
     printf ("Enter time You spend in a shower, please.\n");
-    int waterUsage=129; int showerTime= get_int(); int w = waterUsage * showerTime;
+    int waterUsage=129; int showerTime= get_int(); int w = waterUsage * showerTime; // reciving values and calculating ammount of bottles
     printf ("Water usage for %i minutes of shower is %i onces\n", showerTime, w);
     printf ("This ammount of water is equal to %i bottles of water\n", w/16);
 }// end of main
