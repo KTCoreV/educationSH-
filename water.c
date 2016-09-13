2f5c104a101a22e4c56619524b4e673e7eba4a7c
KTCoreV <cwucore@gmail.com>
2016-09-13 20:25:56 +0300

    Create water.c

---
+++ water.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // add basic C labriray of commands
+#include <cs50.h> // addet aditional labriry writen by Harward
+
+int main (void)
+{
+    printf ("Enter time You spend in a shower, please.\n");
+    int waterUsage=129; int showerTime= get_int(); int w = waterUsage * showerTime;
+    printf ("Water usage for %i minutes of shower is %i onces\n", showerTime, w);
+    printf ("This ammount of water is equal to %i bottles of water\n", w/16);
+}// end of main
