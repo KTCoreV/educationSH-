1dd94f37903403eaca679ceee4ca278ff2691ce2
KTCoreV <cwucore@gmail.com>
2016-09-17 18:08:02 +0300

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -6,8 +6,15 @@ int main (void) {
     printf ("Enter message to code with Ceaser\n"); string msg = get_string();
     printf ("Enter secret number\n"); int n = get_int();
     for (int i=0; i<strlen(msg); i++) {
-    if (msg[i]>n || msg[i]>=256-n) msg[i]=msg[i]-n; // shifting letters to -
-    else msg[i]=msg[i]+n; // shifting letters to +
+            if (msg[i]>=65 && msg[i]<91) { // big letters coding
+            if (msg[i]-n<65) msg[i]=msg[i]-n+26; // shifting letters to - when result is lower then min
+            else msg[i]=msg[i]-n; // shifting letters to -
+        } //if1
+
+        if (msg[i]>=96 && msg[i]<123) { // small letters encoding
+            if (msg[i]-n<96) msg[i]=msg[i]-n+26; // shifting letters to - when result is lower then min
+            else msg[i]=msg[i]-n; // shifting letters to -
+        } //if2
     }//for
-    printf ("\nYour massage is: %s\n", msg);
+    printf ("\nYour massage is:\n%s", msg);
 } //main
