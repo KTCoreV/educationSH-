5f6f508442e81481db0e6f21d9fb4d6d12d1c7c9
KTCoreV <cwucore@gmail.com>
2016-09-22 15:39:41 +0300

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -6,47 +6,38 @@ int main (int wc, string wn[]) { // main recives string thrugh command line
 //chek number of arguments entered in command line
 if (wc<2 || wc>2) {printf ("Error! You must enter a single secret word in command line!"); return 1;}
     // saving word wn[1] in pass variable of type string and counting its length
-    string pass = wn[1]; int wnl = strlen(pass); int i;
+    string pass = wn[1]; int wnl = strlen(pass);
 
 //check if USER entered the latin letters
-for (i=0; i<wnl; i++){
+for (int i=0; i<wnl; i++){
     if (pass[i]<65 || (pass[i]>90 && pass[i]<97) || pass[i]>122){
     printf ("Error. Only latin letters are allowed! Restart."); return 1;}//if2
     }//for wn check
 
 //convert string pass to int array, substract numbers to 0-25 values
 int codeSequance[wnl];
-for (i=0; i<wnl; i++) { 
+for (int i=0; i<wnl; i++) { 
     if (pass[i]<97) codeSequance[i]=(int)pass[i]-65;
     else codeSequance[i]=(int)pass[i]-97;
+   // printf ("Password sequance numer %i is set to: %i\n", i, codeSequance[i]); // for test
     }//for of codeSequance
 
 // USER input of a message to encode
 printf ("Enter message to encode with Vigener\n"); 
 string msg = get_string(); int sl = strlen(msg); // accept message to code and count number of letters
 
-//encoding doble loop for sl and wnl
-int j=0;
-for (i=0; i<sl; i++){
-if (i<sl && j==wnl){j=0;}// loop of wnl code
-    for (; j<wnl; j++){
-        if (msg[i]+codeSequance[i]<65 || (msg[i]+codeSequance[i]>90 && msg[i]+codeSequance[i]<97)){
-            msg[i]=msg[i]+codeSequance[i]+26;
-        }//if msg+codeSequance are beyond MIN limits
-        if ((msg[i]+codeSequance[i]>90 && msg[i]+codeSequance[i]<97) || msg[i]+codeSequance[i]>122){
-            msg[i]=msg[i]+codeSequance[i]-26;
-        }//if msg+codeSequance are beyond MAX limits
-        if ((msg[i]+codeSequance[i]>65 && msg[i]+codeSequance[i]<90) || (msg[i]+codeSequance[i]>97 && msg[i]+codeSequance[i]<122)){
-            msg[i]=msg[i]+codeSequance[i];}
-        if (msg[i]==' ') msg[i]=' ';
-        else msg[i]=msg[i];
-    }//for wnl (shift of letters) loop
 
+//encoding doble loop for sl and wnl
+    int j=-1;//set j iterator for encoding loop to -1, cause in loop I add j++ result j start from 0
+    for (int i=0; i<sl; i++){j++; // here I iterate throgh code Sequance while going throgh msg text
+        if (j==wnl){j=0;}// loop of wnl code
+// cheks for encoding msg letters to letters again
+    if (msg[i]+codeSequance[j]>90 && msg[i]+codeSequance[j]<97){msg[i]=msg[i]+codeSequance[j]+26;}
+    if (msg[i]+codeSequance[j]>122){msg[i]=msg[i]+codeSequance[j]-26;}
+    if ((msg[i]+codeSequance[j]>65 && msg[i]+codeSequance[j]<90) || (msg[i]+codeSequance[j]>97 && msg[i]+codeSequance[j]<122)){
+            msg[i]=msg[i]+codeSequance[j];}
+            
+// printf ("Shift of %i with %i sequance = %i to char %c\n", i, j, codeSequance[j], msg[i]); //for test
 }//for sl (string) loop
      printf ("Encrypted message is:\n %s\n", msg);
-
-//loop for codeSequance
-//chek if msg+codeSequance is > than limits
-// add numbers to msg
-
 return 0;} //main
