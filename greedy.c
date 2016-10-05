a5529499dc856251bbcd40b83f0ee34ea31c55b7
KTCoreV <cwucore@gmail.com>
2016-10-05 13:14:38 +0300

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -1,25 +1,37 @@
 #include <stdio.h> // add basic C labriray of commands
-#include <cs50.h> // addet aditional labriry writen by Harward
+#include <cs50.h> // added aditional labriry of commands writen by Harward
+#include <math.h> // add math library of commands for round the numbers
 
-/* ПРОБЛЕМЫ: (1) при задании нечетных цифр с 3 в конце, ошибочно подсчитывает число пенни (1 коп.) */
 void cashe (void); // anounced function prototype
 
 int main (void) { cashe (); } // main is short cause all job is done in f cashe
 
 void cashe (void) {
     printf ("Enter ammount of change You recive\n"); 
-    float change = get_float(); int n=0; int q=0; int t=0; int f=0; int p=0; // retrive form USER ammount of change at set counters fo coins
-    if (change>=0.001){  // check if USER entered a valid number of change
-        while (change>0.00) {   // loop that asks how much change has left 
-            // if change is greater than a quarter numder of coins is increesed and 25 is substracted from change
-            if (change>=0.25){change=change-0.25; n++; q++;} 
-            // if change is lesser than 25 but greater or equal to 10, give 10th coin and incrise coin counter
-            else if (change<0.25 && change>=0.1) {change=change-0.1; n++; t++;} 
-            // if change is lesser than 10 but greater or equal to 5, give 5th coin and incrise coin counter
-            else if (change<0.1 && change>=0.05) {change=change-0.05; n++; f++;}
-            // if change is lesser than 5, give penny and incrise coin counter. PROBLAM located here!!! If enter non even NUM - wrong ammount of pennies
-            else if (change<0.05) {change=change-0.01; n++; p++;}}
-        
-    } else { printf("Wrong number. :(\n"); cashe ();} // if USER entered a wrong sum function ask a new entery by self-recalling
-    printf ("You recived %i coins\n Quarters: %i, 10th: %i, 5th: %i, penny: %i\n Change remains: %30f\n", n, q, t, f, p, change); // printing number of coins given as change 
-} // end of main 
+     float sum = get_float(); // retrive form USER ammount of change
+    int change = round(sum*100); // multiply float on 100 and round it for propper result
+    int n=0; // ammount of coins
+    
+     if (change>0){  // check if USER entered a valid number of change
+        while (change>0) {   // loop that asks how much change has left 
+       
+                // if change is greater than a quarter numder of coins is increesed and 25 is substracted 
+                // from change
+             if (change>=25){change=change-25; n++; } 
+    
+                // if change is lesser than 25 but greater or equal to 10, give 10th coin and incrise coin counter
+            else if (change<25 && change>=10) {change=change-10; n++; } 
+     
+                // if change is lesser than 10 but greater or equal to 5, give 5th coin and incrise coin counter
+            else if (change<10 && change>=5) {change=change-5; n++; }
+     
+                // if change is lesser than 5, give penny and incrise coin counter. PROBLAM located here!!!
+            else if (change<5 && change>0) {change=change-1; n++; }
+        }//while
+    
+    // if USER entered a wrong sum function ask a new entery by self-recalling
+    } else { printf("Wrong number. :(\n"); cashe ();} 
+    
+    //printing number of coins given as change
+    printf ("%i\n", n);   
+} // end of cashe
